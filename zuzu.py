cae354cbd475c45229684c2196455251583d69c1
Yaroslav Bogdanov <litle32@ya.ru>
2022-05-07 04:39:25 +0500

    added file for smth

--- zuzu.py
+++ zuzu.py
@@ -1,7 +1,7 @@
 import json
 import time
 from pprint import pprint
-
+import pandas as pd
 import requests
 import vk_api
 import config
@@ -10,6 +10,7 @@ import config
 # vk = vk_api.VkApi(token=config.vk_api_user_token)
 # api = vk.get_api()
 def search_user_pair_info(city, age_from, age_to):
+    # Добавить идентификацию - если написала девушка, изменить sex на 2
     params = {
         'access_token': config.vk_api_user_token,
         'v': config.version,
@@ -44,10 +45,51 @@ def search_user_pair_info(city, age_from, age_to):
             yield i
 
 
+# table_info = pd.DataFrame(search_user_pair_info(49, 23, 24))
+# for i in search_user_pair_info(49, 23, 24):
+#     print(i)
 
 
-for user in search_user_pair_info(49, 23, 24):
-    print(user)
+def get_best_three_photo(owner_id):
+    user_url = 'https://api.vk.com/method/' + 'photos.get'
+    user_params = {
+        'access_token': config.vk_api_user_token,
+        'v': config.version,
+        'owner_id': owner_id,
+        'album_id': 'profile',
+        'photo_sizes': '1',
+        'extended': '1'
+    }
+    req = requests.get(user_url, params=user_params).json()
+    most_popular = dict()
+    for photo in req['response']['items']:
+        sizes = photo['sizes']
+        likes = photo['likes']['count']
+        comments = photo['comments']['count']
+        popularity = likes + comments
+        max_size_url = max(sizes, key=get_largest)['url']
+        max_size_photo = max(sizes, key=get_largest)['type']
+        most_popular[popularity] = max_size_url
 
-# with open('pump.json', 'a', encoding='utf-8') as f:
-#     json.dump(user_list, f, indent=2, ensure_ascii=False)
+    new_sorted_dict = dict(sorted(most_popular.items(), key=lambda x: x[0], reverse=True))
+    result = []
+    for key, value in new_sorted_dict.items():
+        if len(result) != 3:
+            result.append(value)
+
+    return result
+
+
+
+def get_largest(size_dict):
+    # Выясняю, горизонтально или вертикально-ориентирована фотография,
+    # что бы далее мог отсортировать по нужному параметру словари
+
+    if size_dict['width'] >= size_dict['height']:
+        return size_dict['width']
+    return size_dict['height']
+
+
+# for i in get_best_three_photo(20246948):
+#     print(i)
+print(get_best_three_photo(20246948))
