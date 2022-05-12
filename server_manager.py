9d0f3cc76f6ca94276766c3ebae71410cf65cca7
Yaroslav Bogdanov <litle32@ya.ru>
2022-05-13 01:55:26 +0500

    fixes

--- server_manager.py
+++ server_manager.py
@@ -3,10 +3,9 @@ from server import Server
 import config
 import sys
 
-server1 = Server(config.vk_api_token, 212988687, "server1")
+server1 = Server(config.vk_api_token, ********, "server1")
 # vk_api_token - API токен, который мы ранее создали
 # 212988687 - id сообщества-бота
 # "server1" - имя сервера
 
 server1.start()
-
