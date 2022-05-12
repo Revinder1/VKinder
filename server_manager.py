06b7ce4c282e2e294f3024ce0fa1c131bcbfd8b2
Yaroslav <67860564+Revinder1@users.noreply.github.com>
2022-05-13 01:57:09 +0500

    Update server_manager.py

--- server_manager.py
+++ server_manager.py
@@ -1,7 +1,6 @@
 from commander import Commander
 from server import Server
 import config
-import sys
 
 server1 = Server(config.vk_api_token, ********, "server1")
 # vk_api_token - API токен, который мы ранее создали
