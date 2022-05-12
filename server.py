d9a8ea9eb3e4823f95082d8714f884bd0d276dd8
Yaroslav Bogdanov <litle32@ya.ru>
2022-05-13 00:08:34 +0500

    fixes

--- server.py
+++ server.py
@@ -48,48 +48,51 @@ class Server:
                               'Прежде чем мы начнем знакомства, укажите возраст в формате Х-Х (Прим.: 18-20)')
 
     def start(self):
-        for event in self.long_poll.listen():  # Слушаем сервер
-            # Пришло новое сообщение
-            if event.type == VkBotEventType.MESSAGE_NEW:
-                new_dict = dict()
-                peer_id = event.object["message"]["peer_id"]
-                from_id = event.object["message"]["from_id"]
-                msg_text = event.object["message"]["text"]
-                msg_type = event.object["message"]["id"]
-                print(event.object)
+        try:
+            for event in self.long_poll.listen():  # Слушаем сервер
+                # Пришло новое сообщение
+                if event.type == VkBotEventType.MESSAGE_NEW:
+                    new_dict = dict()
+                    peer_id = event.object["message"]["peer_id"]
+                    from_id = event.object["message"]["from_id"]
+                    msg_text = event.object["message"]["text"]
+                    msg_type = event.object["message"]["id"]
+                    print(event.object)
 
-                # ПРОВЕРКА НА НАЛИЧИЕ СВЯЗАННОГО ЮЗЕРА-КЛАССА В СЛОВАРЕ
-                if from_id not in self.users:
-                    new_dict[peer_id] = commander.Commander(self.api_token, self.group_id, from_id, peer_id, msg_type)
-                    self.users[from_id] = new_dict
-                    # Отправляем меню новому пользователю
-                    self.menu_bot(from_id, peer_id)
-                    self.users[from_id][peer_id].handle_message(msg_text)
+                    # ПРОВЕРКА НА НАЛИЧИЕ СВЯЗАННОГО ЮЗЕРА-КЛАССА В СЛОВАРЕ
+                    if from_id not in self.users:
+                        new_dict[peer_id] = commander.Commander(self.api_token, self.group_id, from_id, peer_id, msg_type)
+                        self.users[from_id] = new_dict
+                        # Отправляем меню новому пользователю
+                        self.menu_bot(from_id, peer_id)
+                        self.users[from_id][peer_id].handle_message(msg_text)
 
-                # ЕСЛИ ОДИН И ТОТ ЖЕ ЮЗЕР ОТПРАВЛЯЕТ СООБЩЕНИЯ С ЛС И ГРУППЫ, ДОБАВИТЬ В СЛОВАРЬ peer_id ГРУППЫ
-                # И СВЯЗАТЬ С КЛАССОМ
-                elif peer_id not in self.users[from_id]:
-                    # ПРИХОДИТСЯ ОБНОВЛЯТЬ ЗНАЧЕНИЕ В СЛОВАРЕ, ИНАЧЕ ПРИ ОТПРАВКЕ С ЛС/ГРУППЫ АТРИБУТЫ КЛАССА Commander
-                    # НЕ ОБНОВЛЯЮТСЯ И ОТПРАВЛЯЕТСЯ ТОЛЬКО В ОДНО МЕСТО
-                    self.users[from_id][peer_id] = commander.Commander(self.api_token, self.group_id, from_id, peer_id,
-                                                                       msg_type)
-                    self.users[from_id][peer_id].handle_message(msg_text)
+                    # ЕСЛИ ОДИН И ТОТ ЖЕ ЮЗЕР ОТПРАВЛЯЕТ СООБЩЕНИЯ С ЛС И ГРУППЫ, ДОБАВИТЬ В СЛОВАРЬ peer_id ГРУППЫ
+                    # И СВЯЗАТЬ С КЛАССОМ
+                    elif peer_id not in self.users[from_id]:
+                        # ПРИХОДИТСЯ ОБНОВЛЯТЬ ЗНАЧЕНИЕ В СЛОВАРЕ, ИНАЧЕ ПРИ ОТПРАВКЕ С ЛС/ГРУППЫ АТРИБУТЫ КЛАССА Commander
+                        # НЕ ОБНОВЛЯЮТСЯ И ОТПРАВЛЯЕТСЯ ТОЛЬКО В ОДНО МЕСТО
+                        self.users[from_id][peer_id] = commander.Commander(self.api_token, self.group_id, from_id, peer_id,
+                                                                           msg_type)
+                        self.users[from_id][peer_id].handle_message(msg_text)
 
-                else:
-                    self.users[from_id][peer_id].handle_message(msg_text)
+                    else:
+                        self.users[from_id][peer_id].handle_message(msg_text)
 
-            elif event.type == VkBotEventType.MESSAGE_EVENT:
-                if event.object['payload'].get('text') == "Клавиатура отключена":
-                    r = self.vk_api.messages.sendMessageEventAnswer(
-                        event_id=event.object['event_id'],
-                        user_id=from_id,
-                        peer_id=peer_id,
-                        event_data=json.dumps(event.object['payload']))
-                    self.send_msg(peer_id, message='клавиатура отключена',
-                                  keyboard=open('keyboards/turn_off_keyboard.json', "r", encoding="UTF-8").read())
-                else:
-                    self.switcher(event, from_id, peer_id)
-
+                elif event.type == VkBotEventType.MESSAGE_EVENT:
+                    if event.object['payload'].get('text') == "Клавиатура отключена":
+                        r = self.vk_api.messages.sendMessageEventAnswer(
+                            event_id=event.object['event_id'],
+                            user_id=from_id,
+                            peer_id=peer_id,
+                            event_data=json.dumps(event.object['payload']))
+                        self.send_msg(peer_id, message='клавиатура отключена',
+                                      keyboard=open('keyboards/turn_off_keyboard.json', "r", encoding="UTF-8").read())
+                    else:
+                        self.switcher(event, from_id, peer_id)
+        except KeyboardInterrupt:
+            self.send_msg(peer_id, message='клавиатура отключена, До Свидания',
+                          keyboard=open('keyboards/turn_off_keyboard.json', "r", encoding="UTF-8").read())
 
 
     def switcher(self, event, from_id, peer_id):
