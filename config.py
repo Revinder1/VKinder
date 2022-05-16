813cc2a56b54df41ace80b8129ed5450ecd8feb9
Yaroslav <67860564+Revinder1@users.noreply.github.com>
2022-05-16 15:25:08 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,6 @@ vk_api_token = ''
 vk_api_user_token = ''
 version = '5.131'
 group_id = 123456
-db_adress = 'postgresql://postgres:password@localhost:5432/dbname'
+db_user = ''
+db_password =''
+db_name = ''
