86bb22b1bde3491b1d627b34f3fa3e188eef8f41
Yaroslav <67860564+Revinder1@users.noreply.github.com>
2022-05-27 23:17:49 +0500

    Update server_manager.py

--- server_manager.py
+++ server_manager.py
@@ -2,9 +2,10 @@ from commander import Commander
 from server import Server
 import config
 
-server1 = Server(config.vk_api_token, config.group_id, "server1")
+
+if __name__ == '__main__':
+  server1 = Server(config.vk_api_token, config.group_id, "server1")
 # vk_api_token - API токен, который мы ранее создали
 # group_id - id сообщества-бота
 # "server1" - имя сервера
-
-server1.start()
+  server1.start()
